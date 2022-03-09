db30cd8dfea9ad547ef3630c18a0f0cf8eb00057
Mutahhir Khan <mutahhir199@gmail.com>
2022-03-09 16:44:01 +0500

    subgraph issue exists

--- typeB/subgraph-template-method/template-method/src/mapping.ts
+++ typeB/subgraph-template-method/template-method/src/mapping.ts
@@ -4,10 +4,11 @@ import {
 import { ParentEntity, ChildEntity } from "../generated/schema"
 import { Initialize } from './../generated/templates/Child/Child';
 import { log } from '@graphprotocol/graph-ts';
+import {Child} from "./../generated/templates";
 
 export function handleChildGeneration(event: ChildGeneration): void {
   log.info("YAHAN,{}",[""]);
-    log.info("here is the parent {}", [""]);
+  log.info("here is the parent {}", [""]);
   let entity = ParentEntity.load(event.transaction.hash.toHex())
 
   if (!entity) {
@@ -16,22 +17,23 @@ export function handleChildGeneration(event: ChildGeneration): void {
   entity._name = event.params._name;
   entity._parentAddress = event.params._parentAddress
   entity._childNumber = event.params._childNumber
-
+  
   entity.save()
 }
 
 
 export function handleInitialize(event: Initialize): void {
   log.info("YAHAN,{}",[""]);
-    log.info("here is the child {}", [""]);
-  let entity = ChildEntity.load(event.transaction.hash.toHex())
-
+  log.info("here is the child {}", [""]);
+// let entity: ChildEntity = new ChildEntity(event.transaction.hash.toHex());
+  let entity  = ChildEntity.load(event.transaction.hash.toHex())
   if (!entity) {
+    Child.create(event.params.childAddress);    
     entity = new ChildEntity(event.transaction.hash.toHex())
   }
 
-  entity._parentAddress = event.params._parentAddress
-  entity._childAddress = event.params.childAddress
-  entity._name = event.params._name
-  entity.save()
+  // entity._parentAddress = event.params._parentAddress
+  // entity._childAddress = event.params.childAddress
+  // entity._name = event.params._name
+  // entity.save()
 }
